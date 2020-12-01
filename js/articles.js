e653f2eddced6bf082b8224fa084f3ad06e28825
nfe213 <eadsnf@gmail.com>
2020-12-01 14:38:12 -0600

    ... I don't understand ...

---
+++ js/articles.js
@@ -0,0 +1,35 @@
+function BSA() {
+  window.open("https://www.atlantabsa.org/WeAreHelpful");
+}
+
+function Pres() {
+  window.open("https://president.ua.edu/news/message-from-the-president-april-13-2020/");
+}
+
+function Mag() {
+  window.open("https://www.atlantamagazine.com/health/cloth-face-masks-how-to-make-them-where-to-buy-them-and-how-to-donate-them-in-atlanta/");
+}
+
+function News() {
+  window.open("https://www.mdjonline.com/neighbor_newspapers/northside_sandy_springs/community/these-atlanta-and-sandy-springs-residents-and-businesses-are-giving-back-amid-coronavirus-outbreak/article_ec8f6b8e-73a7-11ea-b82d-cf7a400aed83.html");
+}
+
+function tweet() {
+  window.open("https://twitter.com/RICSFoundation/status/1245325394296934400");
+}
+
+function patch() {
+  window.open("https://patch.com/georgia/sandysprings/makerspace-allows-riverwood-students-explore-stem-activities");
+}
+
+function mtb() {
+  window.open("https://patch.com/georgia/sandysprings/riverwood-mountain-biking-team-places-1st-season");
+}
+
+function forKai() {
+  window.open("https://patch.com/georgia/sandysprings/riverwood-s-robotics-team-compete-state-competition");
+}
+
+function updates() {
+  window.open("https://www.gofundme.com/f/atlanta-face-shields");
+}
--- js/hamburger.js
+++ js/hamburger.js
@@ -4,3 +4,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("navOverlay").style.width = "0%";
 }
+
+function navHover() {
+  //document.getElementById('activePage').style.backgroundColor = '#900f09';
+  //document.getElementById('activePage').style.color = 'white';
+}
+
+function navReset() {
+  //document.getElementById('activePage').style.backgroundColor = 'white';
+  //document.getElementById('activePage').style.color = 'black';
+}

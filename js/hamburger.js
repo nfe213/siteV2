81de46ee3f06a0fa482babde682f5048648cd80c
Nathan Eads <eadsnf@gmail.com>
2021-10-01 21:20:35 -0500

    got rid of dumb

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,13 +4,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("navOverlay").style.width = "0%";
 }
-
-function navHover() {
-  //document.getElementById('activePage').style.backgroundColor = '#900f09';
-  //document.getElementById('activePage').style.color = 'white';
-}
-
-function navReset() {
-  //document.getElementById('activePage').style.backgroundColor = 'white';
-  //document.getElementById('activePage').style.color = 'black';
-}
